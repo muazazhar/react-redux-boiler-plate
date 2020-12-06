1f405ecc26223e7944ddb46c75d60e916ece3e29
Muaz Azhar <49091541+muazazhar@users.noreply.github.com>
2020-12-06 11:35:56 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,6 @@ import reducer from "./reducer";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-//can use this also
-// const store = createStore(reducer,{}, applyMiddleware(thunk));
-
 const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
